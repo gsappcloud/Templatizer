79e79992388e7a752aa56128ca0a869cccf7309a
jh mpbnew osx <vomc8one@gmail.com>
2012-06-25 19:02:13 -0400

    includes

--- sites/all/themes/tmpltzr/tmpltzr.js
+++ sites/all/themes/tmpltzr/tmpltzr.js
@@ -1,19 +1,3 @@
-var head = document.getElementsByTagName('head')[0];
-script = document.createElement('script');
-script.src = "http://postfog.org/assets/js/jquery.masonry.min.js";
-script.type = 'text/javascript';
-head.appendChild(script);
-
-script = document.createElement('script');
-script.src = "http://postfog.org/assets/js/fetcher.js";
-script.type = 'text/javascript';
-head.appendChild(script);
-
-script = document.createElement('script');
-script.src = "http://postfog.org/assets/js/jquery.cycle.all.pack.js";
-script.type = 'text/javascript';
-head.appendChild(script);
-
 $(document).ready(function() {
   
   gsappFetcher.start();
