59ced3cf98af29facf5278a65e2b94690126a9a1
jh mpbnew osx <vomc8one@gmail.com>
2012-06-26 14:43:43 -0400

    removing old 'templatizer' theme and adding it as archive

--- sites/all/themes/templatizer/js/templatizer.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-  var view = $('#content-area').html();
-  view = '<textarea>' + view + '</textarea>';
-  $('#block-block-1').append(view);
-  console.log(view);
-});
