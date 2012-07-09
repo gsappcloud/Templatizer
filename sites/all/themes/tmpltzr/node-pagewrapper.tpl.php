0d0bee0094bd85b13578a94c6c62584d4229aa2a
Troy Conrad Therrien <troyth@gmail.com>
2012-07-09 17:16:50 -0400

    added colors based on taxonomy to pagewrapper in htmlelements.css and node-pagewrapper.tpl.php

--- sites/all/themes/tmpltzr/node-pagewrapper.tpl.php
+++ sites/all/themes/tmpltzr/node-pagewrapper.tpl.php
@@ -4,23 +4,46 @@
 	</div>
 <?php } ?>
 
+
+<?php
+	print '<div class="';
+    $terms = taxonomy_node_get_terms_by_vocabulary($node, 9); // vid=9 => color-code
+		if(!empty($terms)) {
+        	foreach ($terms as $term){
+            	print $term->name;
+            }
+        } else {
+            print 'none';
+        }      
+        print '" >';       
+?>
+
 <?php if(!empty($node->field_page_title[0]['view'])){ ?>
 	<h3 class="title"><?php print $node->field_page_title[0]['view']; ?></h3>
 <?php } ?>
 
 
-<?php
-	$viewName = 'Pages';
-	$display_id = 'page_1';
-	print views_embed_view($viewName, $display_id, $node->nid);
-?>
+	<?php
+	/*
+		Use the "Page" view to pull in all the modules associated with this page
+	*/
+		$viewName = 'Pages';
+		$display_id = 'page_1';
+		print views_embed_view($viewName, $display_id, $node->nid);
+	?>
 
-<footer id="page-wrapper-footer">
-	<div id="gsapp-url">
-		<h4>GSAPP website url:</h4>
-		<?php print $node->field_url[0]['view']; ?>
-	</div>
-	<div id="copy-paste">
-		<h4>Copy-paste the code below into the GSAPP website:</h4>
-	</div>
+	<footer id="page-wrapper-footer">
+		<div id="gsapp-url">
+			<h4>GSAPP website url:</h4>
+			<?php print $node->field_url[0]['view']; ?>
+		</div>
+		<div id="copy-paste">
+			<h4>Copy-paste the code below into the GSAPP website:</h4>
+		</div>
+	</footer>
+
 </div>
+
+
+
+
