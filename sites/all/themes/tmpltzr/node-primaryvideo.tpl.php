25b8862b846de9998a5731700601f97f32b06e6a
Troy Conrad Therrien <troyth@gmail.com>
2012-07-11 15:59:53 -0400

    added primaryvideo and secondaryvideo, and changed primaryembed to be html embed code

---
+++ sites/all/themes/tmpltzr/node-primaryvideo.tpl.php
@@ -0,0 +1,36 @@
+<?php if (!$page){ ?>
+<div id="node-<?php print $node->nid; ?>" class="tmpltzr-module tmpltzr-primary tmpltzr-primaryvideo node<?php if ($sticky) { print ' sticky'; } ?><?php if (!$status) { print ' node-unpublished'; } ?> clearfix">
+<a id="<?php print $node->title; ?>" name="<?php print $node->title; ?>" class="anchorhash"></a>
+<?php } ?>
+	
+	<?php if(!empty($node->field_embedded_video)){ ?>
+  		<div class="tmpltzr-embed-video">
+  			<?php print $node->field_embedded_video[0]['view']; ?>
+  		</div>
+  	<?php } ?>
+  	
+  	<?php if(!empty($node->field_embedded_flickr_set)){ ?>
+  		<div class="tmpltzr-embed-flickr">
+  			<?php print $node->field_embedded_flickr_set[0]['view']; ?>
+  		</div>
+  	<?php } ?>
+  	
+  	<?php if(!empty($node->field_title)){ ?>
+  		<div class="tmpltzr-title">
+  			<?php print $node->field_title[0]['view']; ?>
+  		</div>
+  	<?php } ?>
+	
+	<?php if(!empty($node->field_body)){ ?>
+  		<div class="tmpltzr-body">
+  			<?php print $node->field_body[0]['view']; ?>
+  		</div>
+  	<?php } ?>
+  	
+
+<?php if ($user->uid) { ?>
+	<div class="tmpltzr-edit">
+		<a href="http://postfog.org/templatizer/node/<?php print $node->nid; ?>/edit" title="<?php print $node_url; ?>">EDIT THIS SECTION</a>
+	</div>
+<?php } ?>
+</div> <!-- /.node -->
