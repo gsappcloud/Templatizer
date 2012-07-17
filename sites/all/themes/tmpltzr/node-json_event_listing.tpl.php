d81a12bbd98d9283743e385a12d05d0c6a294ab3
jh mpbnew osx <vomc8one@gmail.com>
2012-07-17 18:17:40 -0400

    Empty node template for JSON output from event feeds

---
+++ sites/all/themes/tmpltzr/node-json_event_listing.tpl.php
@@ -0,0 +1,20 @@
+<?php if (!$page): ?>
+  <article id="node-<?php print $node->nid; ?>" class="node<?php if ($sticky) { print ' sticky'; } ?><?php if (!$status) { print ' node-unpublished'; } ?> clearfix">
+<?php endif; ?>
+
+
+    <?php if (!$page): ?>
+      <header>
+        <h2><a href="<?php print $node_url ?>" title="<?php print $title ?>"><?php print $title ?></a></h2>
+      </header>
+	<?php endif; ?>
+
+  <div class="content">
+    <?php print 'CONTENT<br/><br/>' .  $content ?>
+  </div>
+
+  
+
+<?php if (!$page): ?>
+  </article> <!-- /.node -->
+<?php endif;?>
