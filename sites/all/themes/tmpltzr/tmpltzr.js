160edc6328f52ddf1a801f0e91d9715a2c1bbe99
jh mpbnew osx <vomc8one@gmail.com>
2012-07-18 18:10:26 -0400

    Adding style definitions for the widget output

--- sites/all/themes/tmpltzr/tmpltzr.js
+++ sites/all/themes/tmpltzr/tmpltzr.js
@@ -7,7 +7,52 @@ $(document).ready(function() {
   view = '<textarea>' + '<div id="tmpltzr">' + view + '</div><!-- /#tmpltzr -->' + '</textarea>';
   $('#copy-paste').append(view);
   
-	
+  
+  /*
+  
+  
+
+embedded-event-date-box north-america
+
+#tmpltzr .north-america {
+	color:#00D6FF;
+}
+
+#tmpltzr .latin-america {
+	color:#00E652;
+}
+
+#tmpltzr .none {
+
+}
+
+#tmpltzr .south-asia {
+	color:#FF9E00;
+}
+
+#tmpltzr .middle-east {
+	color:#FF00A6;
+}
+
+#tmpltzr .africa {
+	color:#008AFF;
+}
+
+#tmpltzr .europe {
+	color:#AD20FF;
+}
+
+#tmpltzr .asia {
+	color:#FF555A;
+}
+
+#tmpltzr .other {
+	color:gray;
+}
+
+*/
+  
+  
   
   
   
