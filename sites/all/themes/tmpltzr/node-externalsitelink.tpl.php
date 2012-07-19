c2bbffc61d88f00fffa5c2faee3d18167ee57eb6
Troy Conrad Therrien <troyth@gmail.com>
2012-07-18 22:23:22 -0400

    fixed color issues and migrated the remaining .tmpltzr-title to h2

--- sites/all/themes/tmpltzr/node-externalsitelink.tpl.php
+++ sites/all/themes/tmpltzr/node-externalsitelink.tpl.php
@@ -1,19 +1,8 @@
-<?php if (!$page) { ?>
-  <div id="node-<?php print $node->nid; ?>" class="tmpltzr-module tmpltzr-primary tmpltzr-primaryfull node<?php if ($sticky) { print ' sticky'; } ?><?php if (!$status) { print ' node-unpublished'; } ?> clearfix">
-	<a id="<?php print $node->title; ?>" name="<?php print $node->title; ?>" class="anchorhash"></a>
-<?php } ?>
-	
-	<div class="tmpltzr-title">
-  			<?php print $title; ?>
-  		</div>
-	
-	
-	
+	<?php if(!empty($node->field_external_link[0]['view'])){ ?>
+	<div class="term-index-term">
+  		<?php print $node->field_external_link[0]['view']; ?>
+  	</div>
+  	<?php } ?>
 	
 
-<?php if ($user->uid) { ?>
-	<div class="tmpltzr-edit">
-		<a href="http://postfog.org/templatizer/node/<?php print $node->nid; ?>/edit" title="<?php print $node_url; ?>">EDIT THIS SECTION</a>
-	</div>
-<?php } ?>
-</div> <!-- /.node -->
+
