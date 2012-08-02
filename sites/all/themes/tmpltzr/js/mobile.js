e5382f4004415908cb30c8626bdee9b3bef9a983
jh mpbnew osx <vomc8one@gmail.com>
2012-08-02 11:42:02 -0400

    changes from last night

--- sites/all/themes/tmpltzr/js/mobile.js
+++ sites/all/themes/tmpltzr/js/mobile.js
@@ -1,5 +1,51 @@
 $(document).ready(function() {
 
-// add code here later
+	// iphone full screen
+	window.scrollTo(0,0);
+	
+	$('#mobile-switch-bar').css('left', '570px');
 
+	$('#mobile-switch-bar').toggle(function() {
+		// move menu offscreen and show content
+		var offscreen = {
+			'position': 'absolute',
+			'left': '-2000px',
+		};
+		$('#mobile-menu').css(offscreen);
+		$('#mobile-switch-bar').css('left', 0);
+		$('#mobile-content').css('left', '100px');
+	},
+	function() {
+		// move content offscreen and show menu
+		var offscreen = {
+			'position': 'absolute',
+			'left': '-2000px',
+		};
+		$('#mobile-menu').css('left', 0);
+		$('#mobile-switch-bar').css('left', '570px');
+		$('#mobile-content').css(offscreen);
+	});
+	
+	
+	// load content inline via ajax	
+	$('#programs-page').click(function() {
+			$('#mobile-content').load("/templatizer/programs?mobile=false #tmpltzr", function(data) {
+				$(data); var contents = $('#tmpltzr', data).html();
+				$('#mobile-content').html(contents);
+			});
+	});
+	
+	$('#about-deans').click(function() {
+		load_content("/templatizer/about/deans-statement");
+	});
+	
+	
 });
+
+function load_content(url) {
+	var url = [url, '?mobile=false #tmpltzr'].join('');
+	$('#mobile-content').load(url, function(data) {
+		$(data); var contents = $('#tmpltzr', data).html();
+		$('#mobile-content').html(contents);
+	});
+}
--- sites/all/themes/tmpltzr/page.tpl.php
+++ sites/all/themes/tmpltzr/page.tpl.php
@@ -5,35 +5,33 @@
 
 $browser = null;
 $browser = browscap_get_browser();
-print '<!-- BROWSER INFO -->';
-foreach($browser as $key=>$value) {
-	print '<!-- ' . $key . ' = ' . $value . '-->';
-}
-print '<!-- END BROWSER INFO -->';
-
 $is_mobile = FALSE;
 
+// evaluate request URI to force mobile or desktop content
+// used only for ajax requests
+$r_uri = null;
+$r_uri = $_SERVER["REQUEST_URI"];
+$strpos_mobile_true = strpos($r_uri, 'mobile=true');
+$strpos_mobile_false = strpos($r_uri, 'mobile=false');
 
-if (
+
+if ($strpos_mobile_false > 0) {
+	// force non-mobile theme
+	$is_mobile = FALSE;
+} else if ($strpos_mobile_true > 0) {
+	$is_mobile = TRUE;
+} else if ( 
+	// run regular browser detection
 	($browser['ismobiledevice'] == 1) &&
 	((strpos($browser['useragent'], 'iPad') == FALSE))
-
-
+	// add more clauses here as they come up
 ) {
-	
 		$is_mobile = TRUE;
 }
 
 
-
-
-
-
-
-if ($is_mobile === TRUE) {
-	print '<!-- browser is mobile -->'; // switch theme
-	
-?>	
+// force theme based on mobile detection
+if ($is_mobile === TRUE) { ?>	
 <!DOCTYPE html>
 <html class="" lang="<?php print $language->language; ?>" dir="<?php print $language->dir; ?>" xml:lang="<?php print $language->language; ?>"> 
 <head>
@@ -42,30 +40,22 @@ if ($is_mobile === TRUE) {
 	<!-- Set the viewport width to device width for mobile -->
 	<meta name="viewport" content="width=device-width" />
 	<?php print $styles; ?>
-	
 	<link type="text/css" rel="stylesheet" media="all" href="/templatizer/sites/all/themes/tmpltzr/css/html-elements.css" />
 	<link type="text/css" rel="stylesheet" media="all" href="/templatizer/sites/all/themes/tmpltzr/css/tabs.css" />
 	<link type="text/css" rel="stylesheet" media="all" href="/templatizer/sites/all/themes/tmpltzr/css/gsapp.css" />
-	
-	<link type="text/css" rel="stylesheet" media="print" href="/templatizer/sites/all/themes/tmpltzr/css/print.css" />
-	
 	<!--[if IE]>
 	  <link rel="stylesheet" href="<?php print $includes_dir; ?>/ie.css" type="text/css">
 	<![endif]-->
-	
 	<!--[if IE 6]>
 	  <link rel="stylesheet" href="<?php print $includes_dir; ?>/ie6.css" type="text/css">
 	<![endif]-->
-
 	<!-- IE Fix for HTML5 Tags -->
 	<!--[if lt IE 9]>
 		<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
 	<![endif]-->
 	
-	
 	<link type="text/css" rel="stylesheet" media="all" href="/templatizer/sites/all/themes/tmpltzr/css/mobile-specific.css" />
-	
-	
+
 	<?php print $scripts; ?>
 	
 	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.js"></script>
@@ -78,84 +68,89 @@ if ($is_mobile === TRUE) {
   <script type="text/javascript" src="/templatizer/sites/all/themes/tmpltzr/js/jquery.jcarousel.min.js"></script>
   
   
-  <!-- mobile NOT WORKING must be some cache insanity-->
+  <!-- mobile  js -->
 	<script type="text/javascript" src="/templatizer/sites/all/themes/tmpltzr/js/mobile.js"></script>
-
-<script type="text/javascript">
-$(document).ready(function() {
-	$('body').css('color', 'red');
-	
-	
-	
-	$('#mobile-switch-bar').click(function() {
-	
-	});
-	
-	
-	$('#about-deans').click(function() {
-			alert('click about');
-	});
-});
-	</script>
 
 	<!-- js assets for fonts.com custom font DIN -->
 	<script type="text/javascript" src="http://fast.fonts.com/jsapi/83f34eca-2e88-4bbf-b358-062ac880084c.js"></script>
-	
-	<style>
-
-/** 640 x 960 */
-
-#mobile-wrapper {
-	width: 640px;
-	min-height: 960px
-	background-color: black;
-}
-
-#mobile-menu {
-	width: 500px;
-	border: 1px solid black;
-	float: left;
-	background-color: yellow;
-	height: 2000px;
-}
-
-#mobile-switch-bar {
-	float: right;
-	background-color: blue;
-	width: 100px;
-	height: 2000px;
-	
-}
-
-#mobile-content {
-	position: absolute;
-	left: -2000px;
-	height: 2000px;
-	width: 600px;
-	background-color: green;
-	
-}
-	</style>
 </head>
 <body class="<?php print $body_classes;?>">
-
 <div id="mobile-wrapper">
+	<div id="mobile-header">
+		<a href="http://postfog.org/templatizer?mobile=true">
+			<div id="gsapp-logo" class="mobile-header-item">
+			<img src="/templatizer/sites/all/themes/tmpltzr/assets/mobile/gsapp-logo_237x49.jpg" width="237" height="49" alt="GSAPP logo" />
+			</div>
+		</a>
+		<a href="http://news.gsapp.org">
+			<div id="gsapp-news"  class="mobile-header-item">
+				<img src="/templatizer/sites/all/themes/tmpltzr/assets/mobile/gsapp_news_83x81.jpg" width="83" height="81" alt="GSAPP News" />
+			</div>
+		</a>
+		<div id="gsapp-search"  class="mobile-header-item">
+			<img src="/templatizer/sites/all/themes/tmpltzr/assets/mobile/search_56x54.jpg" width="56" height="54" alt="Search" />
+		</div>
+		<div id="gsapp-login"  class="mobile-header-item">
+			<a href="#">Login</a>
+		</div>
+	</div>
 	<div id="mobile-menu">
-		MENU HERE<br/>
-		(non option)<br/>
-		(non option)<br/>
-		(non option)<br/>
-		<a href="#" id="about-deans">ABOUT PAGE</a><br/>
-		(non option)<br/>
-		(non option)<br/>
-		(non option)<br/>
-		<a href="#" id="programs-page">PROGRAMS PAGE</a><br/>
-		(non option)<br/>
-		(non option)<br/>
-		(non option)<br/>
+	<?php
+	
+	function print_children($item, $level = 0) {
+				
+		if ($item['link']['has_children'] > 0) {
+
+			print $item['link']['title'] . '<br/>';
+			
+			foreach($item['below'] as $k=>$v) {
+				print_children($v, $level++);
+			}
+
+		} else {
+			print $level . '- ' . $item['link']['title'] . '<br/>';
+			// go back up
+			$level--;
+			
+
+		}	
+	}
+
+
+
+
+	$pl = menu_tree_all_data('primary-links');
+	print '<ul>';
+	
+	
+	
+
+	foreach($pl as $key=>$value) {
+		
+		print_children($value);
+		
+	}
+	
+	
+	
+	print '</ul>';
+	
+	
+	?>
+	
+		<ul>
+			<li><a href="#" id="about-deans">ABOUT</a></li>
+			<li><a href="#" id="programs-page">PROGRAMS</a></li>
+			<li><a href="#" >STUDIO-X-GLOBAL</a></li>
+			<li><a href="#" >CENTERS</a></li>
+		</ul>			
 	</div>
 	<div id="mobile-switch-bar">
-&lt;<br/>&lt;<br/>&lt;<br/>&lt;<br/>&lt;<br/>&lt;<br/>&lt;<br/>&lt;<br/>
+		<div>
+		<img src="/templatizer/sites/all/themes/tmpltzr/assets/mobile/view_page_arrow_26x33.jpg" width="26" height="33" alt="VIEW PAGE" />
+		<img src="/templatizer/sites/all/themes/tmpltzr/assets/mobile/view_page_26x155.jpg" width="26" height="155" alt="VIEW PAGE" />
+		<img src="/templatizer/sites/all/themes/tmpltzr/assets/mobile/view_page_arrow_26x33.jpg" width="26" height="33" alt="VIEW PAGE" />
+		</div>
 	</div>
 	<div id="mobile-content">
 		<?php print $content; ?>
@@ -268,11 +263,11 @@ $(document).ready(function() {
 		<section id="content" class="clearfix">
 			<?php if ($tabs): ?><div class="tabs"><?php print $tabs; ?></div><?php endif; ?>
 			<?php print $messages . $help . $header; ?>
-			
+			<!--starttmpltzr-->
 			<div id="tmpltzr">
-				<?php print $content; ?>
-				<div id="right-sidebar"></div>
+					<?php print $content; ?>
 			</div>
+			<!--endtmpltzr-->
 				
 			<!-- Footer -->
 			<footer id="footer">
